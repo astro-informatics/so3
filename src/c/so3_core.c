cc810b473bb32129716684395a3c923df0e8f983
Martin Buettner <martin.buettner.11@ucl.ac.uk>
2013-06-30 23:22:33 +0000

    # Continued implementation of inverse transform (still WIP).

--- src/c/so3_core.c
+++ src/c/so3_core.c
@@ -37,43 +37,63 @@
  */
 void so3_core_mw_inverse_via_ssht(complex double *f, const complex double *flmn,
 	int L, int N,
+        so3_storage_t storage,
 	int verbosity)
 {
-    int i, n;
+    int i, el, n;
     int nsqr;
     int ind;
     complex double *fn, *flm;
+    int offset;
+    int fn_n_stride;
 
     // Print messages depending on verbosity level.
     if (verbosity > 0) {
         printf("%sComputing inverse transform using MW sampling with\n", SO3_PROMPT);
         printf("%sparameters  (L, N, reality) = (%d, %d, FALSE)\n", SO3_PROMPT, L, N);
         if (verbosity > 1)
-            printf("%sUsing routine so3_core_mw_inverse_via_ssht...\n", SO3_PROMPT);
+            printf("%sUsing routine so3_core_mw_inverse_via_ssht with storage method %d...\n"
+                    , SO3_PROMPT
+                    , storage);
     }
     
     // Compute fn(a,b)
-    fn = (complex double*)calloc((2*N-1)*L*(2*L-1), sizeof(complex double));
+    fn_n_stride = L * (2*L-1);
+    // We make fn large enough so that the components with n < 0 can fit in twice.
+    // This is utilized later to do a spatial shift via a simple memcpy().
+    fn = (complex double*)malloc((3*N-2)*fn_n_stride * sizeof(complex double));
     flm = (complex double*)calloc(L*L, sizeof(complex double));
-    for(n = -N+1; n < N; n++)
+    for(n = -N+1; n < N; ++n)
     {
         nsqr = n*n;
 
-        // Pull out 1D flm array from flmn and left-pad with 0s.
-        for(i = 0; i < nsqr; n++)
-            flm[i] = 0.0;
+        so3_sampling_elmn2ind(&ind, 0, 0, n, L, N, SO3_STORE_ZERO_FIRST_PAD);
+        memcpy(flm, flmn + ind, L*L);
         
-        so3_sampling_elmn2ind(&ind, abs(n), -abs(n), n, L, N, SO3_STORE_ZERO_FIRST_PAD);
-        memcpy(flm + nsqr, flmn, L*L - nsqr);
-        
-        
+        offset = 0;
+        i = 0;
+        for(el = 0; el < L; ++el)
+        {
+            for (; i < offset + 2*el+1; ++i)
+                flm[i] *= sqrt(double(2*el+1)/(16.*pow(SO3_PI, 3.));
+
+            offset = i;
+        }
+
+        ssht_core_mw_inverse_sov_sym(fn + (n + N-1)*fn_n_stride, flm, L, -n, SSHT_DL_TRAPANI, verbosity);
     }
+
+    // Apply spatial shift
+    memcpy(fn + 2*N*fn_n_stride, fn, (N-1)*fn_n_stride * sizeof(complex double));
+
+    // TODO: IFFT
 }
 
     
 
 void so3_core_mw_forward_via_ssht(complex double *flmn, const complex double *f,
 	int L, int N,
+        so3_storage_t storage,
 	int verbosity)
 {
 
