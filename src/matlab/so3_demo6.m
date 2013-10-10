79a1d078fff9dcf8aca1ee996308fc44ce83b26a
Martin Buettner <martin.buettner.11@ucl.ac.uk>
2013-10-10 15:55:30 +0000

    Changed the covariance demo so that the covariance is averaged over multiple realisations of flmn.

--- src/matlab/so3_demo6.m
+++ src/matlab/so3_demo6.m
@@ -1,7 +1,10 @@
 % so3_demo6 - Run demo6
 %
 % Demo to compare theoretical covariance of signal with empirical data from
-% using our transform functions.
+% using our transform functions. The empirical covariance is computed for 
+% several sets of harmonic coefficients, and the theoretical covariance is
+% compared to the mean of those calculations in units of its standard
+% deviation.
 %
 % Default usage is given by
 %
@@ -20,17 +23,11 @@ clear all;
 L = 32;
 N = 3;
 
-% Generate normally distributed random flmn of complex signal
-% with mean 0 and variance 1
-%stream = RandStream.getGlobalStream;
-%reset(stream);
-flmn = zeros((2*N-1)*(3*L^2-N*(N-1))/3, 1);
-flmn = (randn(size(flmn)) + 1i*randn(size(flmn)))/sqrt(2);
-
-var_flmn = var(flmn);
+var_flmn = 1; % Should we use the actual variance var(flmn) of each
+              % realization here instead?
 
 % Compute theoretical variance of signal.
-var_f_theory = 0;%zeros(1, L);
+covar_f_theory = 0; %zeros(1, L);
 dl = zeros(2*L-1, 2*L-1);
 for l = 0:L-1,
     scale = ((2*l+1)/(8*pi^2))^2;
@@ -47,21 +44,35 @@ for l = 0:L-1,
                    % Similarly we leave out the phase of the dlmn.
                    dlmn = dlmn + dl(L+mp,L+m)*dl(L+mp,L+n);%*exp(1i*mp*pi*(2*b+1)/(2*L-1));
                 end
-                %var_f_theory(b+1) = var_f_theory(b+1) + scale*abs(dlmn)^2;
-                var_f_theory = var_f_theory + scale*abs(dlmn)^2;
+                %covar_f_theory(b+1) = covar_f_theory(b+1) + scale*abs(dlmn)^2;
+                covar_f_theory = covar_f_theory + scale*abs(dlmn)^2;
             %end
         end
     end
 end
 
-var_f_theory = var_f_theory .* var_flmn
+covar_f_theory = covar_f_theory .* var_flmn
 
-% Compute inverse then forward transform.
-f = so3_inverse(flmn, L, N, 'Storage', 'Compact');
-%f = permute(f, [2 3 1]);
-%f = reshape(f, [L, (2*L-1)*(2*N-1)]).';
+runs = 100;
+covar_f_data = zeros(runs,1);
+    
+for i = 1:runs,
+    % Generate normally distributed random flmn of complex signal
+    % with mean 0 and variance 1
+    %stream = RandStream.getGlobalStream;
+    %reset(stream);
+    flmn = zeros((2*N-1)*(3*L^2-N*(N-1))/3, 1);
+    flmn = (randn(size(flmn)) + 1i*randn(size(flmn)))/sqrt(2);
 
-%var_f_data = var(f)
-var_f_data = var(f(:))
+    % Compute inverse then forward transform.
+    f = so3_inverse(flmn, L, N, 'Storage', 'Compact');
+    % Reshape f, so that var(f) will give variance for each beta.
+    %f = permute(f, [2 3 1]);
+    %f = reshape(f, [L, (2*L-1)*(2*N-1)]).';
 
-max_err = max(abs(var_f_theory - var_f_data))
+    %covar_f_data(i,:) = var(f)
+    covar_f_data(i) = var(f(:));
+end
+mean_covar_f_data = mean(covar_f_data);
+std_covar_f_data = std(covar_f_data);
+error_in_std = abs(mean_covar_f_data - covar_f_theory)/std_covar_f_data
