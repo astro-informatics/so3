df2e3f40c3bd790063bcd70f6a5f2a38169ddef8
Martin Büttner <m.buettner.d@gmail.com>
2013-12-18 10:22:01 +0100

    Fixed test script files
    The estimated times for the shell scripts did not take into account that each test is performed 10 times.

--- timings_errors_12d.sh
+++ timings_errors_12d.sh
@@ -1,2 +1,2 @@
 ./bin/c/so3_test_csv 4096 0 4 > timings_errors_L_4096_N_4.csv
-./bin/c/so3_test_csv 512 > timings_errors_L_N_512.csv
+./bin/c/so3_test_csv 1024 > timings_errors_L_N_1024.csv
---
+++ timings_errors_22h.sh
@@ -0,0 +1,2 @@
+./bin/c/so3_test_csv 2048 0 4 > timings_errors_L_2048_N_4.csv
+./bin/c/so3_test_csv 512 > timings_errors_L_N_512.csv
