ba376d2d5eabf1a43bddb2fd7e288e26d66cfd0c
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2019-12-03 21:56:00 +0000

    Add python building stuff

--- setup.py
+++ setup.py
@@ -1,44 +1,32 @@
-import sys
-import os
-import shutil
+from skbuild import setup
 
-from distutils.core import setup, Extension
-from Cython.Distutils import build_ext
-from Cython.Build import cythonize
-
-import numpy
-
-# clean previous build
-for root, dirs, files in os.walk('./src/python/', topdown=False):
-    for name in dirs:
-        if (name == 'build'):
-            shutil.rmtree(name)
-
-include_dirs = [
-    numpy.get_include(),
-    './include/c',
-    os.environ['SSHT'] + '/src/c',
-]
-
-extra_link_args = [
-    '-L./build',
-    '-L' + os.environ['FFTW'] + '/lib',
-    # depending whether used cmake
-    '-L' + os.environ['SSHT'] + '/build',
-    # or make
-    '-L' + os.environ['SSHT'] + '/lib/c',
+cmake_args = [
+    "-Dpython:BOOL=ON",
+    "-Dtests:BOOL=OFF",
+    "-Dconan_fftw=ON",
 ]
 
 setup(
-    name='pyso3',
-    version='2.0',
-    cmdclass={'build_ext': build_ext},
-    ext_modules=cythonize([Extension(
-        'src/python/pyso3',
-        sources=['src/python/pyso3.pyx'],
-        include_dirs=include_dirs,
-        libraries=['so3', 'ssht', 'fftw3'],
-        extra_link_args=extra_link_args,
-        extra_compile_args=[]
-    )])
+    name="pyso3",
+    version="2.0",
+    author="Jason McEwen",
+    install_requires=["numpy", "cython", "scipy"],
+    extras_require={
+        "dev": [
+            "setuptools",
+            "wheel",
+            "scikit-build",
+            "cmake",
+            "ninja",
+            "cython",
+            "conan",
+        ]
+    },
+    description="Fast spin spherical transforms",
+    url="http://astro-informatics.github.io/ssht/",
+    package_dir={"pyso3": "src/pyso3"},
+    cmake_args=cmake_args,
+    cmake_languages=("C",),
+    license="GPL-2",
+    packages=["pyso3"],
 )
