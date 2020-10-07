64a4d1d29bd815aec6fde04e31b688a42d035bf7
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2020-10-07 22:09:29 +0100

    Remove use of inline since it leads to errors on clang
    Though it may actually be kosher, if not useful.

--- include/so3_sampling.h
+++ include/so3_sampling.h
@@ -23,13 +23,11 @@ double so3_sampling_a2alpha(int a, const so3_parameters_t* parameters);
 double so3_sampling_b2beta(int b, const so3_parameters_t* parameters);
 double so3_sampling_g2gamma(int g, const so3_parameters_t* parameters);
 
-// Note, if this is compiled using C99-standard then the "extern" belongs in the
-// .c file instead.
-extern inline int so3_sampling_flmn_size(const so3_parameters_t* parameters);
-extern inline void so3_sampling_elmn2ind(int* ind, int el, int m, int n, const so3_parameters_t* parameters);
-extern inline void so3_sampling_ind2elmn(int* el, int* m, int* n, int ind, const so3_parameters_t* parameters);
-extern inline void so3_sampling_elmn2ind_real(int* ind, int el, int m, int n, const so3_parameters_t* parameters);
-extern inline void so3_sampling_ind2elmn_real(int* el, int* m, int* n, int ind, const so3_parameters_t* parameters);
+int so3_sampling_flmn_size(const so3_parameters_t* parameters);
+void so3_sampling_elmn2ind(int* ind, int el, int m, int n, const so3_parameters_t* parameters);
+void so3_sampling_ind2elmn(int* el, int* m, int* n, int ind, const so3_parameters_t* parameters);
+void so3_sampling_elmn2ind_real(int* ind, int el, int m, int n, const so3_parameters_t* parameters);
+void so3_sampling_ind2elmn_real(int* el, int* m, int* n, int ind, const so3_parameters_t* parameters);
 
 #ifdef __cplusplus
 }
