4d03cc7d99607c9f96a17924b4bb32373ec42266
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2020-11-24 22:27:34 +0000

    Update ssht to 1.3.4

--- conanfile.py
+++ conanfile.py
@@ -25,7 +25,7 @@ class So3Conan(ConanFile):
 
     def requirements(self):
         location = "astro-informatics/stable" if self.in_local_cache else "user/testing"
-        self.requires(f"ssht/1.3.3@{location}")
+        self.requires(f"ssht/1.3.4@{location}")
 
     def configure(self):
         if self.settings.compiler == "Visual Studio":
