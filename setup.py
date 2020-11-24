d956dde5493c9e9ccf9129c534e43ed691cd9616
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2020-11-24 22:27:58 +0000

    Bump to 1.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ cmake_args = ["-Dtests:BOOL=OFF"]
 
 setup(
     name="so3",
-    version="1.3.0",
+    version="1.3.1",
     author="Jason McEwen",
     install_requires=["numpy", "cython", "scipy"],
     extras_require={
