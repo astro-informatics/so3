14ff02cba377cfcd745cbba5d38d227053ddb4fa
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2021-05-12 17:06:38 +0100

    Modify namespace scheme for conan-center

--- cmake/exporting.cmake
+++ cmake/exporting.cmake
@@ -1,18 +1,19 @@
 # Exports so3 so other packages can access it
 export(
-  TARGETS so3
-  FILE "${PROJECT_BINARY_DIR}/So3Targets.cmake"
-  NAMESPACE so3)
+  TARGETS astro-informatics-so3
+  FILE "${PROJECT_BINARY_DIR}/astro-informatics-so3Targets.cmake"
+  NAMESPACE astro-informatics-so3::)
 
 # Avoids creating an entry in the cmake registry.
 if(NOT NOEXPORT)
-  export(PACKAGE so3)
+  export(PACKAGE astro-informatics-so3)
 endif()
 
 set(INCLUDE_INSTALL_DIR include/)
 include(CMakePackageConfigHelpers)
 configure_package_config_file(
-  cmake/so3Config.in.cmake "${PROJECT_BINARY_DIR}/so3Config.cmake"
+  cmake/so3Config.in.cmake
+  "${PROJECT_BINARY_DIR}/astro-informatics-so3Config.cmake"
   INSTALL_DESTINATION lib/cmake/so3
   PATH_VARS INCLUDE_INSTALL_DIR)
 write_basic_package_version_file(
@@ -21,12 +22,12 @@ write_basic_package_version_file(
   COMPATIBILITY SameMajorVersion)
 
 if(NOT CONAN_EXPORTED)
-  install(FILES "${PROJECT_BINARY_DIR}/so3Config.cmake"
-                "${PROJECT_BINARY_DIR}/so3ConfigVersion.cmake"
-          DESTINATION lib/cmake/so3)
+  install(FILES "${PROJECT_BINARY_DIR}/astro-informatics-so3Config.cmake"
+                "${PROJECT_BINARY_DIR}/astro-informatics-so3ConfigVersion.cmake"
+          DESTINATION lib/cmake/astro-informatics-so3)
 endif()
 
 install(
   EXPORT so3Targets
-  DESTINATION lib/cmake/so3
-  NAMESPACE so3)
+  DESTINATION lib/cmake/astro-informatics-so3
+  NAMESPACE astro-informatics-so3::)
