f8d52fc89562c3f69607a13932d68ae1191fc3a5
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2021-09-29 09:05:16 +0100

    Conan remote is conancenter and sundries

--- cmake/so3Config.in.cmake
+++ cmake/so3Config.in.cmake
@@ -6,7 +6,7 @@ include(CMakeFindDependencyMacro)
 find_dependency(FFTW3 REQUIRED)
 find_dependency(Ssht REQUIRED)
 
-include("${CMAKE_CURRENT_LIST_DIR}/sshtTargets.cmake")
-set(SO3_LIBRARIES so3::so3)
+include("${CMAKE_CURRENT_LIST_DIR}/astro-informatics-so3Targets.cmake")
+set(SO3_LIBRARIES astro-informatics-so3::astro-informatics-so3)
 
 check_required_components(SO3)
